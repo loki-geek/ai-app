80746439e768dec20e133d623c5e89213cc34ffd
Logesh <lokiverse101@proton.me>
2024-01-13 17:00:51 +0530

    Removed Api Key

--- chatbot_app/gemini.py
+++ chatbot_app/gemini.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 inputMsg = input("Enter your prompt: ")
 
-API_KEY = 'AIzaSyBLAuKEfR5QsUMpn8SANRUMcc8gNQZQstU'
+API_KEY = ''
 gemini_api_key = API_KEY
 genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel('gemini-pro')
--- chatbot_app/recipe_app/views.py
+++ chatbot_app/recipe_app/views.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 # Create your views here.
 def ask_gemini(mealType, foodType, ingredientItems):
         ModifiedInput=f"You are an Intelligent Recipe recommender. I want you to recommend a delicious recipe for {mealType}. Remember that I prefer {foodType} food. Here are the ingredients: {ingredientItems}. Please provide the suitable recipe. Note: Word Limit is 150 words" 
-        API_KEY = 'AIzaSyBLAuKEfR5QsUMpn8SANRUMcc8gNQZQstU'
+        API_KEY = ''
         gemini_api_key = API_KEY
         genai.configure(api_key=gemini_api_key)
         model = genai.GenerativeModel('gemini-pro')
